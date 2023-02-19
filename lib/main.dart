093ad18a21716eb53618dca9c7b542dc7f557e3f
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-02-19 18:08:32 +0300

    Виджет Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,25 @@ import 'package:flutter/material.dart';
 // точка входа в программу
 void main() {
   // создание и вывод на экран виджета
-  runApp(const MaterialApp(
-    home: Center(
-        child: Text(
-          "Hello Flutter",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-              color: Colors.red, fontSize: 28.0, fontWeight: FontWeight.bold),
+  runApp(
+    MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.indigo,
+      appBar: AppBar(
+        title: const Text("Shadowhunters"),
+        centerTitle: true,
         ),
-      ),
+        body: const Center(
+          child: Text(
+            "This is Homepage Shadowhunters",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.white
+              ),
+              ),
+              ),
+        ),
     ),
   );
 }
