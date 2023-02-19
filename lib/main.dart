08f29afd0baef89427f16da45421d2553d13c5dc
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-02-19 17:39:22 +0300

    Виджет MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 // точка входа в программу
 void main() {
   // создание и вывод на экран виджета
-  runApp(
-     const Center(
-      child: Text(
-        "Hello Flutter",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.red, fontSize: 28.0, fontWeight: FontWeight.bold),
+  runApp(const MaterialApp(
+    home: Center(
+        child: Text(
+          "Hello Flutter",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              color: Colors.red, fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
       ),
     ),
   );
