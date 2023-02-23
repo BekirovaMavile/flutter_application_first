b6736557a91e7e4bf484cca57e3605a3c00132a1
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-02-23 17:58:54 +0300

    Assets and Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,29 @@
-import 'dart:async';
+// import 'dart:async';
 
 import 'package:flutter/material.dart';
 
 // точка входа в программу
-void main() => runApp(FirstApp());
+void main() => runApp(MyApp());
 
-class FirstApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _FirstAppState();
-  }
-}
-
-class _FirstAppState extends State<FirstApp> {
-  bool _loading = false;
-  double _progressValue = 0.0;
-
-  @override
-  void initState() {
-    _loading;
-    _progressValue;
-    super.initState();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.indigo,
         appBar: AppBar(
-          title: Text("My First App"),
-          centerTitle: true,
+          title: Text('Adding Assets'),
         ),
         body: Center(
-          child: Container(
-            padding: EdgeInsets.all(16),
-            child: _loading
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      LinearProgressIndicator(value: _progressValue),
-                      Text(
-                        '${(_progressValue * 100).round()}%',
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                    ],
-                  )
-                : Text(
-                    'Press button to download',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _loading = !_loading;
-              _updateProgress();
-            });
-          },
-          child: Icon(Icons.cloud_download),
-        ),
+            child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Image(
+              image: AssetImage('assets/images/11.jpeg'),
+            ),
+            Image.asset('assets/icons/22.png'),
+          ],
+        )),
       ),
     );
-  }
-
-  void _updateProgress() {
-    const oneSec = const Duration(seconds: 1);
-    Timer.periodic(oneSec, (Timer t) {
-      setState(() {
-        _progressValue += 0.2;
-
-        if (_progressValue.toStringAsFixed(1) == '1.0') {
-          _loading = false;
-          t.cancel();
-          _progressValue = 0.0;
-          return;
-        }
-      });
-    });
   }
 }
