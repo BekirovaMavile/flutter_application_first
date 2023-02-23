5fc7bebd58b7013fd127bf9a8cf4a948ea942da1
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-02-23 18:14:37 +0300

    Assets and Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme:ThemeData(fontFamily: 'TiltNeon'),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Adding Assets'),
         ),
         body: Center(
-            child: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Image(
-              image: AssetImage('assets/images/11.jpeg'),
-            ),
-            Image.asset('assets/icons/22.png'),
-          ],
-        )),
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Image(
+                image: AssetImage('assets/images/11.jpeg'),
+              ),
+              Image.asset('assets/icons/22.png'),
+              Positioned(
+                top: 16,
+                left: 115,
+                child: Text(
+                  "My custom font",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    // fontFamily: 'TiltNeon',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
