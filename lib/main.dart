19948a5a876ea76c187b8f2349393a54b5687029
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-03-04 15:56:08 +0300

    Counter Stl Stf

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,95 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/color_bloc.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Counter',
-      home: MyHomePage(),
+      title: 'Mavi',
+      theme: ThemeData.dark(),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, this.title = 'Counter', this.centerTitle = true});
+
+  final String title;
+  final bool centerTitle;
+
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  ColorBloc _bloc = ColorBloc();
+class _HomePageState extends State<HomePage> {
+  int _counter = 50;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    _counter--;
+    setState(() {});
+  }
 
   @override
-  void dispose(){
-    _bloc.dispose();
-    super.dispose();
-  }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BLoC with Stream'),
+        title: Text(widget.title),
+        centerTitle: widget.centerTitle,
       ),
-    body: Center(
-      child: StreamBuilder(
-        stream: _bloc.outputStateStream,
-        initialData: Colors.red,
-        builder: (context, snapshot){
-          return AnimatedContainer(
-            height: 100,
-            width: 100,
-            color: snapshot.data,
-            duration: const Duration(milliseconds: 500)
-          );
-        },
-      )
-    ),
-    floatingActionButton: Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        FloatingActionButton(
-          backgroundColor: Colors.red,
-          onPressed: () {
-            _bloc.inputEventSink.add(ColorEvent.event_red);
-          },
+      body: Container(
+        color: Colors.green,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                '"-" to decrement',
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              Container(
+                width: 130,
+                margin:
+                    const EdgeInsets.symmetric(vertical: 10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.indigo[50],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                        onPressed: _decrementCounter,
+                        icon: const Icon(Icons.remove),
+                        color: Colors.black),
+                    Text(
+                      '$_counter',
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                    IconButton(
+                        onPressed: _incrementCounter,
+                        icon: const Icon(Icons.add),
+                        color: Colors.black),
+                  ],
+                ),
+              ),
+              const Text(
+                '"+" to increment',
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              )
+            ],
+          ),
         ),
-        const SizedBox(width: 10),
-        FloatingActionButton(
-          backgroundColor: Colors.green,
-          onPressed: () {
-            _bloc.inputEventSink.add(ColorEvent.event_green);
-          },
-        )
-      ],
       ),
     );
   }
